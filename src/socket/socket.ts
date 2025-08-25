5fbdef809aaf0a413d8db69c5dad4c9b4c5dce53
viswajith k k <viswajithkanayi@gmail.com>
2025-08-25 11:24:30 +0530

    url changed backend

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -73,7 +73,11 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:5173",
-    origin: "https://www.viswajith.site",
+     origin: [
+      "https://www.viswajith.site",
+      "https://viswajith.site",   
+      "http://localhost:5173"    
+    ],
     credentials: true,
   },
 });
